0ea403cec1c6c6829ec1d6e49bfb3585593c8667
Elias Carneiro de Oliveira <elolivei@gmail.com>
2024-09-07 11:55:06 -0300

    Respondendo ao desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,43 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    fun incluirConteudo(conteudo: ConteudoEducacional){
+        conteudos.add(conteudo)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val variaveis = ConteudoEducacional("Lidando com variaveis")
+    val desvios = ConteudoEducacional("Desvios condicionais",120)
+    val cursoPython  = Formacao( "Python", mutableListOf(variaveis,desvios))
+    val aluno = Usuario("Joao")
+    cursoPython.matricular(aluno)
+    
+    println(cursoPython)
+    println(cursoPython.inscritos)
+    
+    val loops = ConteudoEducacional("loops de repeticao")
+    cursoPython.incluirConteudo(loops)
+    
+    val aluno1 = Usuario("Matilde de Souza")
+    cursoPython.matricular(aluno1)
+    println()
+    println(cursoPython)
+    println(cursoPython.inscritos)
+    
 }
